327e1c1ea416a7604f84486bf1815ea2e7b78386
reset42 <fragezeichen42@googlemail.com>
2025-08-04 13:46:11 +0200

    Delete state.py

--- state.py
+++
@@ -1,3 +0,0 @@
-SUCCESS = 1           # Alles lief problemlos
-RECOVERED = 2         # Fehler, aber intern gelöst (z. B. reconnect)
-FATAL_ERROR = 3       # Schwerer Fehler, nicht automatisch gelöst
