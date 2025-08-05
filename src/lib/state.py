1e7b24ab69c2d7d76e929bdf553f4057a56e952b
reset42 <fragezeichen42@googlemail.com>
2025-08-05 11:36:39 +0200

    Delete src directory

--- src/lib/state.py
+++
@@ -1,4 +0,0 @@
-#state.py
-SUCCESS = 1           # Alles lief problemlos
-RECOVERED = 2         # Fehler, aber intern gelöst (z. B. reconnect)
-FATAL_ERROR = 3       # Schwerer Fehler, nicht automatisch gelöst
